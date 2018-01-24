b43c158407f7a0596591c9428fe7d45cd2c70646
XenHat <XenHat@users.noreply.github.com>
2018-01-24 14:37:01 -0500

    Comment out unused CPUStats class

--- Program.cs
+++ Program.cs
@@ -137,90 +137,90 @@ namespace TrayApp
             }
         }
 
-        private class CPUStats
-        {
-            public static float currentCPUUsage;
-            protected static PerformanceCounter cpuCounter;
-            protected static PerformanceCounter ramCounter;
-            private static List<float> AvailableCPU = new List<float>();
-            private static List<float> AvailableRAM = new List<float>();
-            private static int cores = 0;
-            private static List<PerformanceCounter> cpuCounters = new List<PerformanceCounter>();
+        //private class CPUStats
+        //{
+        //    public static float currentCPUUsage;
+        //    protected static PerformanceCounter cpuCounter;
+        //    protected static PerformanceCounter ramCounter;
+        //    private static List<float> AvailableCPU = new List<float>();
+        //    private static List<float> AvailableRAM = new List<float>();
+        //    private static int cores = 0;
+        //    private static List<PerformanceCounter> cpuCounters = new List<PerformanceCounter>();
 
-            public static void ConsumeCPU()
-            {
-                int percentage = 60;
-                if (percentage < 0 || percentage > 100) //-V3022
-                    throw new ArgumentException("percentage");
-                Stopwatch watch = new Stopwatch();
-                watch.Start();
-                while (true)
-                {
-                    // Make the loop go on for "percentage" milliseconds then sleep the remaining
-                    // percentage milliseconds. So 40% utilization means work 40ms and sleep 60ms
-                    if (watch.ElapsedMilliseconds > percentage)
-                    {
-                        Thread.Sleep(100 - percentage);
-                        watch.Reset();
-                        watch.Start();
-                    }
-                }
-            }
+        //    public static void ConsumeCPU()
+        //    {
+        //        int percentage = 60;
+        //        if (percentage < 0 || percentage > 100) //-V3022
+        //            throw new ArgumentException("percentage");
+        //        Stopwatch watch = new Stopwatch();
+        //        watch.Start();
+        //        while (true)
+        //        {
+        //            // Make the loop go on for "percentage" milliseconds then sleep the remaining
+        //            // percentage milliseconds. So 40% utilization means work 40ms and sleep 60ms
+        //            if (watch.ElapsedMilliseconds > percentage)
+        //            {
+        //                Thread.Sleep(100 - percentage);
+        //                watch.Reset();
+        //                watch.Start();
+        //            }
+        //        }
+        //    }
 
-            public static void TimerElapsed(object source, ElapsedEventArgs e)
-            {
-                float cpu = cpuCounter.NextValue();
-                float sum = 0;
-                foreach (PerformanceCounter c in cpuCounters)
-                {
-                    sum = sum + c.NextValue();
-                }
-                sum = sum / (cores);
-                float ram = ramCounter.NextValue();
-                Console.WriteLine(string.Format("CPU Value 1: {0}, cpu value 2: {1} ,ram value: {2}", sum, cpu, ram));
-                AvailableCPU.Add(sum);
-                AvailableRAM.Add(ram);
-                currentCPUUsage = sum;
-            }
+        //    public static void TimerElapsed(object source, ElapsedEventArgs e)
+        //    {
+        //        float cpu = cpuCounter.NextValue();
+        //        float sum = 0;
+        //        foreach (PerformanceCounter c in cpuCounters)
+        //        {
+        //            sum = sum + c.NextValue();
+        //        }
+        //        sum = sum / (cores);
+        //        float ram = ramCounter.NextValue();
+        //        Console.WriteLine(string.Format("CPU Value 1: {0}, cpu value 2: {1} ,ram value: {2}", sum, cpu, ram));
+        //        AvailableCPU.Add(sum);
+        //        AvailableRAM.Add(ram);
+        //        currentCPUUsage = sum;
+        //    }
 
-            private static void Main(string[] args)
-            {
-                cpuCounter = new PerformanceCounter();
-                cpuCounter.CategoryName = "Processor";
-                cpuCounter.CounterName = "% Processor Time";
-                cpuCounter.InstanceName = "_Total";
+        //    private static void Main(string[] args)
+        //    {
+        //        cpuCounter = new PerformanceCounter();
+        //        cpuCounter.CategoryName = "Processor";
+        //        cpuCounter.CounterName = "% Processor Time";
+        //        cpuCounter.InstanceName = "_Total";
 
-                foreach (var item in new System.Management.ManagementObjectSearcher("Select * from Win32_Processor").Get())
-                {
-                    cores = cores + int.Parse(item["NumberOfCores"].ToString());
-                }
+        //        foreach (var item in new System.Management.ManagementObjectSearcher("Select * from Win32_Processor").Get())
+        //        {
+        //            cores = cores + int.Parse(item["NumberOfCores"].ToString());
+        //        }
 
-                ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        //        ramCounter = new PerformanceCounter("Memory", "Available MBytes");
 
-                int procCount = System.Environment.ProcessorCount;
-                for (int i = 0; i < procCount; i++)
-                {
-                    System.Diagnostics.PerformanceCounter pc = new System.Diagnostics.PerformanceCounter("Processor", "% Processor Time", i.ToString());
-                    cpuCounters.Add(pc);
-                }
+        //        int procCount = System.Environment.ProcessorCount;
+        //        for (int i = 0; i < procCount; i++)
+        //        {
+        //            System.Diagnostics.PerformanceCounter pc = new System.Diagnostics.PerformanceCounter("Processor", "% Processor Time", i.ToString());
+        //            cpuCounters.Add(pc);
+        //        }
 
-                Thread c = new Thread(ConsumeCPU);
-                c.IsBackground = true;
-                c.Start();
+        //        Thread c = new Thread(ConsumeCPU);
+        //        c.IsBackground = true;
+        //        c.Start();
 
-                try
-                {
-                    System.Timers.Timer t = new System.Timers.Timer(1200);
-                    t.Elapsed += new ElapsedEventHandler(TimerElapsed);
-                    t.Start();
-                    Thread.Sleep(10000);
-                }
-                catch (Exception e)
-                {
-                    ExceptionHandler(e);
-                }
-                Console.ReadLine();
-            }
-        }
+        //        try
+        //        {
+        //            System.Timers.Timer t = new System.Timers.Timer(1200);
+        //            t.Elapsed += new ElapsedEventHandler(TimerElapsed);
+        //            t.Start();
+        //            Thread.Sleep(10000);
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            ExceptionHandler(e);
+        //        }
+        //        Console.ReadLine();
+        //    }
+        //}
     }
 }
